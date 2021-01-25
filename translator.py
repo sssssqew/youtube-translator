6107d5fb57a4c652338649a21b26c4a2b9d441ec
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 21:20:31 +0900

    added multiple language translating

--- translator.py
+++ translator.py
@@ -2,29 +2,56 @@
 #  구글 번역 API 다운로드 하기 => pip3 install google_trans_new
 # 구글 번역 라이브러리는 Python >=3.6 에서 동작함
 
+import os
 from google_trans_new import google_translator
 
+directory_to_save_captions = '/home/sylee/dev/utility/youtube-translator/captions'
+
+# 자막 파일들을 담을 폴더 생성하기
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+ 
+createFolder(directory_to_save_captions)
+
+
+filename = 'captions.srt' # 유튜브 영상에서 다운받은 자막파일 이름
 src_lang = 'ko'
-tgt_lang = 'ja' # 변경할 언어코드만 바꿔주면 된다
+tgt_lang_array = ['en', 'zh-cn', 'zh-tw', 'hi', 'ru',
+                             'ja', 'de', 'th', 'id', 'ms', 'vi', 'tl',
+                             'fi', 'fr', 'pl',  'pt', 'es', 'tr', 'it',
+                             'uk', 'da', 'no', 'nl', 'sv', 'he'
+                             ]
 
 translator = google_translator()  
 
-#  파일 전체 읽어서 lines 배열에 저장하기
-f = open("captions.srt", "r")
-lines = f.readlines()
 
-# 라인마다 순회하면서 한글이 있는 줄만 번역한 다음 lines 배열에 업데이트하기
-for i, line in enumerate(lines):
-  if (i-2) % 4 == 0:
-    # print(lines[i])
-    translate_text = translator.translate(lines[i],lang_src=src_lang, lang_tgt=tgt_lang)  
-    print(translate_text)
-    lines[i] = translate_text + "\n" # 원래 다운로드한 자막파일에 자막마다 한줄 띄워쓰기가 있어서 반드시 "\n"  있어야 함
+def translate_and_save_into_file(filename, tgt_lang):
+  #  파일 전체 읽어서 lines 배열에 저장하기
+  f = open(filename, "r")
+  lines = f.readlines()
 
-# 변경된 lines 배열을 다시 파일에 쓰기
-f = open("captions_{}.srt".format(tgt_lang), "w")
-f.writelines(lines)
-f.close()
+  # 라인마다 순회하면서 한글이 있는 줄만 번역한 다음 lines 배열에 업데이트하기
+  for i, line in enumerate(lines):
+    if (i-2) % 4 == 0:
+      # print(lines[i])
+      translate_text = translator.translate(lines[i],lang_src=src_lang, lang_tgt=tgt_lang)  
+      print(translate_text)
+      lines[i] = translate_text + "\n" # 원래 다운로드한 자막파일에 자막마다 한줄 띄워쓰기가 있어서 반드시 "\n"  있어야 함
+
+  # 변경된 lines 배열을 다시 파일에 쓰기
+  f = open("{}/captions_{}.srt".format(directory_to_save_captions, tgt_lang), "w")
+  f.writelines(lines)
+  f.close()
+
+# 번역할 언어 갯수만큼 순회하면서 번역하고 파일 저장함
+for i in range(len(tgt_lang_array)):
+  translate_and_save_into_file(filename, tgt_lang_array[i])
+  print("---------------------------------------------------------------")
+
 
 ############### 변경할 언어 코드 ################
 # 영어 : en
